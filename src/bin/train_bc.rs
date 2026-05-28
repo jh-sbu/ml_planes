b90f64d03411d083aed2e98e2d3a4eea6988598c
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2026-05-28 11:36:45 -0400

    BC Timing Prinout
    - Vibed more information into the stdout printouts for bc training

--- src/bin/train_bc.rs
+++ src/bin/train_bc.rs
@@ -262,7 +262,7 @@ where
         for epoch in 0..bc_epochs {
             last_mse = trainer.pretrain_bc(&data, 1, minibatch);
             if epoch % print_every == 0 || epoch + 1 == bc_epochs {
-                println!("  epoch {:>4}/{bc_epochs}  mse {last_mse:.6}", epoch + 1);
+                println!("  epoch {:>4}/{bc_epochs}  mse {last_mse:.6}  {:.1}s", epoch + 1, train_start.elapsed().as_secs_f64());
             }
         }
         println!(
